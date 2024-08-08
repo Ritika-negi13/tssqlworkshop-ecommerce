775071af507ca7efa83afd21cf6d4c851dfff01d
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-08 12:49:36 +0530

    build folder

--- build/service/deleteproduct.js
+++ build/service/deleteproduct.js
@@ -4,11 +4,16 @@ exports.deleteProduct = deleteProduct;
 const products_1 = require("../model/products");
 function deleteProduct(name) {
     let i = products_1.products.findIndex((val) => val.name.toLocaleLowerCase() == name.toLocaleLowerCase());
-    if (i == -1) {
-        console.log("Product does not exsist ");
+    try {
+        if (i == -1) {
+            throw new Error("Product does not exsist ");
+        }
+        else {
+            products_1.products.splice(i, 1);
+            console.log("Product deleted");
+        }
     }
-    else {
-        products_1.products.splice(i, 1);
-        console.log("Product deleted");
+    catch (e) {
+        console.log(e.message);
     }
 }
--- build/service/searchproduct.js
+++ build/service/searchproduct.js
@@ -5,11 +5,16 @@ const products_1 = require("../model/products");
 const deleteproduct_1 = require("./deleteproduct");
 function searchProduct(name) {
     let i = products_1.products.findIndex((val) => val.name.toLocaleLowerCase() == name.toLocaleLowerCase());
-    if (i == -1) {
-        console.log("Product does not exsist ");
+    try {
+        if (i == -1) {
+            throw new Error("Product does not exsist ");
+        }
+        else {
+            console.log(products_1.products[i]);
+            (0, deleteproduct_1.deleteProduct)(name);
+        }
     }
-    else {
-        console.log(products_1.products[i]);
-        (0, deleteproduct_1.deleteProduct)(name);
+    catch (e) {
+        console.log(e.message);
     }
 }
