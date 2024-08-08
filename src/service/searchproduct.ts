6c6631979b2fea1463b480a8196c3a97f582e016
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-08 12:48:58 +0530

    modified: exception handling added

--- src/service/searchproduct.ts
+++ src/service/searchproduct.ts
@@ -3,13 +3,18 @@ import { deleteProduct } from "./deleteproduct";
 
 export function searchProduct(name:string){
     let i=products.findIndex((val)=>val.name.toLocaleLowerCase()==name.toLocaleLowerCase())
-    if(i==-1)
-    {
-        console.log("Product does not exsist ")
+    try{
+        if(i==-1)
+        {
+            throw new Error("Product does not exsist ")
+        }
+        else
+        {
+            console.log(products[i]);
+            deleteProduct(name);
+        }
     }
-    else
-    {
-        console.log(products[i]);
-        deleteProduct(name);
+    catch(e:any){
+        console.log(e.message)
     }
 }
