86fe0fe09ba0556b57f88aaf176e08fe18b67e59
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-08 12:38:51 +0530

    products file contain product list in json format

---
+++ src/model/products.ts
@@ -0,0 +1,38 @@
+import { Product } from "./product";
+export var products:Product[]=[
+    {
+        name: 'Blazer',
+        code: '123',
+        type: 'Sneakers',
+        color: "White",
+        brand: "Nike"
+    },
+    {
+        name: 'Air Force',
+        code: '125',
+        type: 'Shoes',
+        color: "White",
+        brand: "Nike" 
+    },
+    {
+        name: ' Air Jordan',
+        code: '124',
+        type: 'Shoes',
+        color: "white and black",
+        brand: "Nike"
+    },
+    {
+        name: 'Air Force 1',
+        code: '1212',
+        type: 'Sneakers',
+        color: "Black",
+        brand: "Nike" 
+    },
+    {
+        name: 'Alpha Bounce',
+        code: '1223',
+        type: 'Running Shoes',
+        color: "White",
+        brand: "Adidas" 
+    },
+];
