ce39f6ace41ddd96eac645261aa284a4cee32ddb
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-08 12:18:09 +0530

    product entity

--- src/model/product.ts
+++ src/model/product.ts
@@ -3,12 +3,14 @@ export class Product{
     name!:string;
     type:string;
     color:string;
+    brand:string;
     //availableSize?: string[];
-    avgrating?:string;
-    constructor(code:string,name:string,type:string,color:string){
+    //avgrating?:string;
+    constructor(code:string,name:string,brand:string,type:string,color:string){
             this.code=code;
             this.name=name;
             this.type=type;
             this.color=color;
+            this.brand=brand;
     }
 }
